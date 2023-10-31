bd073af83b868b2872555029cf9f4cd7d298a50e
TetianaShev <tanishika362@gmail.com>
2023-10-31 22:37:46 +0200

    start
    start

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function getCat() {
+const BASE_URL = 'http://api.thecatapi.com/v1';
+const API_KEY = 'live_W2QknaHdg2S5JZXYNQtSjqXyMb4zf7T3kHI3cfAyZFxA0GAjzLZwrrKEOdG5ECpN';
+
+fetch('${'BASE_URL'})
+}
